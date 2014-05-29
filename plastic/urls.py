dcbd963287179e4c3f2e2380651949b5425411ab
leevg <leevg@ukr.net>
2014-05-29 11:43:35 +0300

    Update urls.py

--- plastic/urls.py
+++ plastic/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns(
